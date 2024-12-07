d66597e3970a1894a52618c8698ff86939c4974c
AdrielCareglio <adrielcaregliodev@gmail.com>
2024-12-07 12:53:58 +0100

    template registro

---
+++ proyecto-peliculas/src/components/RegisterForm.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      <h2>Registro</h2>
+      <form @submit.prevent="register"> <!--Formulario html que evita que la web se recargue al eviar el fomulario-->
+        <input v-model="email" type="email" placeholder="Email" required />
+        <input v-model="password" type="password" placeholder="Contraseña" required />
+        <button type="submit">Registrarse</button>
+      </form>
+    </div>
+  </template>
+  
