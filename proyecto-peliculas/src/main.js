9f86698af446ce4d7c43bc22df67c6acb41a94be
AdrielCareglio <adrielcaregliodev@gmail.com>
2024-12-07 12:47:01 +0100

    Modificacion Main para integrar VueFire y Firebase

--- proyecto-peliculas/src/main.js
+++ proyecto-peliculas/src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue';
+/*import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App).mount('#app');*/
+
+import { createApp } from 'vue';
+import { VueFire, VueFireAuth } from 'vuefire'; // Importado de VueFire y autenticación
+import { firebaseApp } from './firebaseConfig'; // Importado de la configuración de firebase
+import App from './App.vue';
+
+const app = createApp(App);
+
+app.use(VueFire, {
+  firebaseApp, // Conexión con firebase
+  modules: [
+    VueFireAuth(),
+  ],
+});
+
+app.mount('#app');
+
